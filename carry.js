2d83441f7eedf251f3dfbcc9aae19e3cae1ea45e
shriram <guraleshriram25@gmail.com>
2020-12-16 22:20:25 +0530

    intial commit

---
+++ carry.js
@@ -0,0 +1,44 @@
+function grating(name , gratingtime="this is morning"){
+    console.log(name + " " + gratingtime);
+    //console.log(name + " wellocme to pune");
+
+}
+
+console.log("hi, this is shriram");
+
+let name="shriram";
+let name1="Sk";
+let name2="Dinesh";
+let name3="Ravi";
+let name4="Akash";
+//let grateingtime;
+let grateingtime="good morning";
+
+grating(name,grateingtime);
+grating(name1,grateingtime);
+grating(name2,grateingtime);
+grating(name3,grateingtime);
+grating(name4);
+
+
+
+function add(a,b,c){
+    let d=a+b*c;
+    return d;
+}
+
+let q=add(1,2,3);
+console.log(q);
+
+function weekday(names){
+    console.log(names);
+}
+
+let names="sunday";
+
+weekday(names);
+// console.log(name + "wellcome to pune");
+// console.log(name1 + "wellcome to pune");
+// console.log(name2 + "wellcome to pune");
+// console.log(name3 + "wellcome to pune");
+// console.log(name4 + "wellcome to pune");
